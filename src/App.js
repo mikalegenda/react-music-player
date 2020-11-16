e2a37d0a65ac1992306f3c839444ccda4c95d60e
mikalegenda <miroslavmihnjakk@gmail.com>
2020-11-16 12:31:41 +0100

    Structured the layout of the app, added some styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+import SongInformation from "./components/SongInformation";
+import Player from "./components/Player";
+import "./styles/app.scss";
+
 function App() {
   return (
     <div className="App">
-      <h1>Music Player</h1>
+      <SongInformation />
+      <Player />
     </div>
   );
 }
